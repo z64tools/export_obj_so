067c2bfbf965e2d629fb4919c40c17337402b7ac
Dragorn421 <Dragorn421@users.noreply.github.com>
2022-02-17 22:44:33 +0100

    Remove most of what was written to .mtl, only keep newmtl and map_Kd

--- export_obj_so.py
+++ export_obj_so.py
@@ -95,111 +95,23 @@ def write_mtl(scene, filepath, path_mode, copy_set, mtl_dict):
             mat_wrap = node_shader_utils.PrincipledBSDFWrapper(mat) if mat else None
 
             if mat_wrap:
-                use_mirror = mat_wrap.metallic != 0.0
-                use_transparency = mat_wrap.alpha != 1.0
+                tex_wrap = getattr(mat_wrap, "base_color_texture", None)
+                if tex_wrap is None:
+                    continue
+                image = tex_wrap.image
+                if image is None:
+                    continue
 
-                # XXX Totally empirical conversion, trying to adapt it
-                #     (from 1.0 - 0.0 Principled BSDF range to 0.0 - 900.0 OBJ specular exponent range)...
-                spec = (1.0 - mat_wrap.roughness) * 30
-                spec *= spec
-                fw("Ns %.6f\n" % spec)
-
-                # Ambient
-                if use_mirror:
-                    fw(
-                        "Ka %.6f %.6f %.6f\n"
-                        % (mat_wrap.metallic, mat_wrap.metallic, mat_wrap.metallic)
-                    )
-                else:
-                    fw("Ka %.6f %.6f %.6f\n" % (1.0, 1.0, 1.0))
-                fw("Kd %.6f %.6f %.6f\n" % mat_wrap.base_color[:3])  # Diffuse
-                # XXX TODO Find a way to handle tint and diffuse color, in a consistent way with import...
-                fw(
-                    "Ks %.6f %.6f %.6f\n"
-                    % (mat_wrap.specular, mat_wrap.specular, mat_wrap.specular)
-                )  # Specular
-                # Emission, not in original MTL standard but seems pretty common, see T45766.
-                emission_strength = mat_wrap.emission_strength
-                emission = [emission_strength * c for c in mat_wrap.emission_color[:3]]
-                fw("Ke %.6f %.6f %.6f\n" % tuple(emission))
-                fw("Ni %.6f\n" % mat_wrap.ior)  # Refraction index
-                fw("d %.6f\n" % mat_wrap.alpha)  # Alpha (obj uses 'd' for dissolve)
-
-                # See http://en.wikipedia.org/wiki/Wavefront_.obj_file for whole list of values...
-                # Note that mapping is rather fuzzy sometimes, trying to do our best here.
-                if mat_wrap.specular == 0:
-                    fw("illum 1\n")  # no specular.
-                elif use_mirror:
-                    if use_transparency:
-                        fw("illum 6\n")  # Reflection, Transparency, Ray trace
-                    else:
-                        fw("illum 3\n")  # Reflection and Ray trace
-                elif use_transparency:
-                    fw(
-                        "illum 9\n"
-                    )  # 'Glass' transparency and no Ray trace reflection... fuzzy matching, but...
-                else:
-                    fw("illum 2\n")  # light normally
-
-                #### And now, the image textures...
-                image_map = {
-                    "map_Kd": "base_color_texture",
-                    "map_Ka": None,  # ambient...
-                    "map_Ks": "specular_texture",
-                    "map_Ns": "roughness_texture",
-                    "map_d": "alpha_texture",
-                    "map_Tr": None,  # transmission roughness?
-                    "map_Bump": "normalmap_texture",
-                    "disp": None,  # displacement...
-                    "refl": "metallic_texture",
-                    "map_Ke": "emission_color_texture"
-                    if emission_strength != 0.0
-                    else None,
-                }
-
-                for key, mat_wrap_key in sorted(image_map.items()):
-                    if mat_wrap_key is None:
-                        continue
-                    tex_wrap = getattr(mat_wrap, mat_wrap_key, None)
-                    if tex_wrap is None:
-                        continue
-                    image = tex_wrap.image
-                    if image is None:
-                        continue
-
-                    filepath = io_utils.path_reference(
-                        image.filepath,
-                        source_dir,
-                        dest_dir,
-                        path_mode,
-                        "",
-                        copy_set,
-                        image.library,
-                    )
-                    options = []
-                    if key == "map_Bump":
-                        if mat_wrap.normalmap_strength != 1.0:
-                            options.append("-bm %.6f" % mat_wrap.normalmap_strength)
-                    if tex_wrap.translation != Vector((0.0, 0.0, 0.0)):
-                        options.append("-o %.6f %.6f %.6f" % tex_wrap.translation[:])
-                    if tex_wrap.scale != Vector((1.0, 1.0, 1.0)):
-                        options.append("-s %.6f %.6f %.6f" % tex_wrap.scale[:])
-                    if options:
-                        fw(
-                            "%s %s %s\n"
-                            % (key, " ".join(options), repr(filepath)[1:-1])
-                        )
-                    else:
-                        fw("%s %s\n" % (key, repr(filepath)[1:-1]))
-
-            else:
-                # Write a dummy material here?
-                fw("Ns 500\n")
-                fw("Ka 0.8 0.8 0.8\n")
-                fw("Kd 0.8 0.8 0.8\n")
-                fw("Ks 0.8 0.8 0.8\n")
-                fw("d 1\n")  # No alpha
-                fw("illum 2\n")  # light normally
+                filepath = io_utils.path_reference(
+                    image.filepath,
+                    source_dir,
+                    dest_dir,
+                    path_mode,
+                    "",
+                    copy_set,
+                    image.library,
+                )
+                fw("map_Kd %s\n" % repr(filepath)[1:-1])
 
 
 def test_nurbs_compat(ob):
